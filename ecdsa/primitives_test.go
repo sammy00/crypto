4765a6979aff11a2297e50eb7d078c11a3b79bf8
sammy00 <xiangminli@sjtu.edu.cn>
2018-05-08 21:27:55 +0800

    add test with ANS1 encoding for private keys

--- ecdsa/primitives_test.go
+++ ecdsa/primitives_test.go
@@ -2,11 +2,19 @@ package ecdsa_test
 
 import (
 	"crypto/rand"
+	"encoding/asn1"
+	"math/big"
 	"testing"
 
 	"github.com/sammy00/crypto/ecdsa"
 	"github.com/sammy00/crypto/elliptic"
+	"golang.org/x/crypto/sha3"
 )
+
+// ecdsaSig serves for testing ANS1 marshaling in signing/verification with private keys
+type ecdsaSig struct {
+	R, S *big.Int
+}
 
 func TestSECP256K1(t *testing.T) {
 	t.Run("secp256k1", func(t *testing.T) {
@@ -18,6 +26,36 @@ func TestSignAndVerify(t *testing.T) {
 	t.Run("secp256k1", func(t *testing.T) {
 		testSignAndVerify(t, elliptic.P256k1())
 	})
+}
+
+func TestSignAndVerifyWithASN1(t *testing.T) {
+	// configure the parameters for curves
+	curve := elliptic.P256k1()
+	// generate the priv-pub key pairs
+	privKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+
+	msg := []byte("Hello World")
+	// get the message digest, a.k.a. its digest
+	digest := sha3.Sum256(msg)
+
+	// sign the message with the key and
+	// get the signature encoded according to ASN1
+	asn1Sig, err := privKey.Sign(rand.Reader, digest[:], nil)
+	if nil != err {
+		t.Fatal(err)
+	}
+
+	// unmarshal the signature for verification
+	var decodedSig ecdsaSig
+	if _, err := asn1.Unmarshal(asn1Sig, &decodedSig); nil != err {
+		t.Fatal(err)
+	}
+
+	// verify the signature (r,s) on the digest
+	// by the corresponding public key
+	if !ecdsa.Verify(&privKey.PublicKey, digest[:], decodedSig.R, decodedSig.S) {
+		t.Fatal("the signature should be valid")
+	}
 }
 
 func TestZeroHashSignature(t *testing.T) {
