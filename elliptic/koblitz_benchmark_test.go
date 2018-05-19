f09743e01def5fb553289e9a7c54954061b4c3bd
sammy00 <xiangminli@sjtu.edu.cn>
2018-05-19 12:34:16 +0800

    add benchmark for KoblitzCurve.ScalarBaseMult

--- elliptic/koblitz_benchmark_test.go
+++ elliptic/koblitz_benchmark_test.go
@@ -3,6 +3,9 @@ package elliptic_test
 import (
 	"testing"
 
+	goelliptic "crypto/elliptic"
+	"crypto/rand"
+
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/sammy00/crypto/elliptic"
 )
@@ -50,6 +53,51 @@ func BenchmarkDouble(b *testing.B) {
 		bb.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
 				koblitzLocal.Double(Gx, Gy)
+			}
+		})
+	})
+	b.Run("Go-ECDSA", func(bb *testing.B) {
+		curve := goelliptic.P256()
+
+		bb.ResetTimer()
+		bb.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				curve.Double(Gx, Gy)
+			}
+		})
+	})
+}
+
+func BenchmarkScalarBaseMult(b *testing.B) {
+	k := make([]byte, 24)
+	rand.Read(k)
+
+	b.Run("BTC", func(bb *testing.B) {
+		koblitzBTC := btcec.S256()
+		bb.ResetTimer()
+		bb.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				koblitzBTC.ScalarBaseMult(k)
+			}
+		})
+	})
+	b.Run("Local", func(bb *testing.B) {
+		koblitzLocal := elliptic.P256k1()
+
+		bb.ResetTimer()
+		bb.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				koblitzLocal.ScalarBaseMult(k)
+			}
+		})
+	})
+	b.Run("Go-ECDSA", func(bb *testing.B) {
+		curve := goelliptic.P256()
+
+		bb.ResetTimer()
+		bb.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				curve.ScalarBaseMult(k)
 			}
 		})
 	})
